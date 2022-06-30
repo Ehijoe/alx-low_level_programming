041349d0d276f022189d192e3297a153962a0164
Joseph Alikah <alikahjoseph@gmail.com>
2022-06-30 13:49:47 +0100

    Add task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+       	fwrite(text, strlen(text), 1, stderr);
+	return (0);
+}
