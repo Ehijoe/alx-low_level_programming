0769be54c4dc7df89c78396d013ef982f61ddf41
Joseph Alikah <alikahjoseph@gmail.com>
2022-06-30 13:55:08 +0100

    Fix task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@ int main(void)
 {
 	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(text, strlen(text) + 1, 1, stderr);
+	fwrite(text, strlen(text), 1, stderr);
 	return (1);
 }
