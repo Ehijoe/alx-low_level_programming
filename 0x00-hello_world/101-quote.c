43223bc9dade11ecbfdcdec622640aae234af938
Joseph Alikah <alikahjoseph@gmail.com>
2022-06-30 13:53:56 +0100

    Add newline in task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fwrite(text, strlen(text) + 1, 1, stderr);
 	return (1);
