b59b52a5e05a1f6eaa9a832adee54b8ca870df84
Joseph Alikah <alikahjoseph@gmail.com>
2022-07-01 12:11:41 +0100

    Add task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	char letter;
+	int n;
 
-	for (letter = '0'; letter <= '9'; letter++)
+	for (n = 0; n < 10; n++)
 	{
-		putchar(letter);
+		putchar('0' + n);
 	}
 	putchar('\n');
 
