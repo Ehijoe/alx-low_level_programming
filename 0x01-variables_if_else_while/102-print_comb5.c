9d499057dc8af4b361f755671ff5cc8709407dd5
Joseph Alikah <alikahjoseph@gmail.com>
2022-07-01 14:31:26 +0100

    Fix task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,9 +13,9 @@ int main(void)
 	{
 		for (j = i; j < 10; j++)
 		{
-			for (k = i; k < 10; k++)
+			for (k = (j == 9) ? i + 1 : i; k < 10; k++)
 			{
-				for (l = j + 1; l < 10; l++)
+				for (l = (j == 9) ? j : j + 1; l < 10; l++)
 				{
 					putchar('0' + i);
 					putchar('0' + j);
