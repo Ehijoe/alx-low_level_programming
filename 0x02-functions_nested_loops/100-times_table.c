3bb046b5d7c71011b84cb4bae51858a2bd1b0b8b
Joseph Alikah <alikahjoseph@gmail.com>
2022-07-05 19:35:49 +0100

    Add task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_times_table - Print the times table from 0 to n
+ * @n: Limit to stop the times table
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				int val = i * j;
+
+				_putchar(',');
+				_putchar(' ');
+				_putchar((val >= 100) ? (val / 100) + '0' : ' ');
+				val %= 100;
+				_putchar((val >= 10) ? (val / 10) + '0' : ' ');
+				_putchar('0' + (val % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
