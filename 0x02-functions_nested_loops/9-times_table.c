c4b00524e374253c483e0c26d4af1bcd7f4b2945
Joseph Alikah <alikahjoseph@gmail.com>
2022-07-05 19:02:49 +0100

    Add task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - Print the times table from 0 to 9
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				int val = i * j;
+
+				_putchar(',');
+				_putchar(' ');
+				_putchar((val > 10) ? (val / 10) + '0' : ' ');
+				_putchar('0' + (val % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
