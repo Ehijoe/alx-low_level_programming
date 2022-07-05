d8611f862e2fcc03f1e6b40fa765bdf6996ae46f
Joseph Alikah <alikahjoseph@gmail.com>
2022-07-05 16:57:34 +0100

    Add task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,36 @@
+int _putchar(char c);
+int print(char *string);
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print("_putchar");
+
+	return (0);
+}
+
+
+/**
+ * print - Print a string to standard output followed by a newline
+ * @string: The string to print
+ *
+ * Return: The number of characters printed to standard output.
+ */
+int print(char *string)
+{
+	int printed = 0;
+
+	while (string[printed])
+	{
+		_putchar(string[printed++]);
+	}
+	_putchar('\n');
+	printed++;
+
+	return (printed);
+}
