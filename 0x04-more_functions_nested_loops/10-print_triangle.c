0ad5648babaea520f07e022109905efc6c93d254
Joseph Alikah <alikahjoseph@gmail.com>
2022-07-07 17:17:12 +0100

    Add task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print a triangle made of #
+ * @size: Height of triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < (n - i - 1); j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j < i + 1; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
