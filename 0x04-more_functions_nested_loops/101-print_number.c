d213882144ad8ccc0736819b2a013d3389f2a20e
Joseph Alikah <alikahjoseph@gmail.com>
2022-07-07 21:16:53 +0100

    Fix task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,14 +11,35 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
+		_putchar('-');
 		print_number(n * -1);
 		return;
 	}
 
-	if (n > 10)
+	if (n >= 10)
 	{
 		print_number(n / 10);
 	}
 
 	_putchar('0' + (n % 10));
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
 }
