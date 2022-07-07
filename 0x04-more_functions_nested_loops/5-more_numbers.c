aa58a9890bbe2def98f23027aa7b800ca68a807a
Joseph Alikah <alikahjoseph@gmail.com>
2022-07-07 16:12:12 +0100

    Add task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,15 +5,18 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i <= 14; i++)
+	for (j = 0; j < 10; j++)
 	{
-		if (i >= 10)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar('0' + (i / 10));
+			if (i >= 10)
+			{
+				_putchar('0' + (i / 10));
+			}
+			_putchar('0' + (i % 10));
 		}
-		_putchar('0' + (i % 10));
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
