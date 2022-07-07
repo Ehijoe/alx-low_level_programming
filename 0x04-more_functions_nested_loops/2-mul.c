e1c9d0b019c780f930ac222bee0b33c48abe72f6
Joseph Alikah <alikahjoseph@gmail.com>
2022-07-07 13:01:26 +0100

    Add task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Add two numbers
+ * @a: First number to add
+ * @b: Second number to add
+ *
+ * Return: The sum of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
