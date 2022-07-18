1b8b2e6f071bf39a663bcd29e2ca09af0596edaa
Joseph Alikah <alikahjoseph@gmail.com>
2022-07-18 09:56:50 +0100

    Add task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * contains - Check if a character is in a string
+ * @s: String to check
+ * @c: Character to look for
+ *
+ * Return: 1 if c is found and 0 otherwise.
+ */
+int contains(char *s, char c)
+{
+	char *ptr = s;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr == c)
+		{
+			return (1);
+		}
+		ptr++;
+	}
+
+	return (0);
+}
+
+/**
+ * _strpbrk - Finds the first occurence of a character in a string
+ * @s: String to search
+ * @accept: String of characters to find
+ *
+ * Return: A pointer to the first occurrence of a character in accept.
+ *         NULL if c is not found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *ptr = s;
+
+	while (!contains(accept, *ptr))
+	{
+		if (*ptr == '\0')
+		{
+			return (NULL);
+		}
+		ptr++;
+	}
+
+	return (ptr);
+}
