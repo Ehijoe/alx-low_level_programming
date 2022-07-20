95f10c2c539550e5a81b513eabcd1c9985c90ade
Joseph Alikah <alikahjoseph@gmail.com>
2022-07-20 16:24:57 +0100

    Fi task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,18 +15,11 @@ int wildcmp_helper(char *s1, char *s2, int wildcard)
 		if (*s1 == *s2)
 		{
 			if (wildcmp_helper(s1, s2, 0))
-			{
 				return (1);
-			}
-			else
-			{
-				return (wildcmp_helper((s1 + 1), s2, 1));
-			}
+			return (wildcmp_helper((s1 + 1), s2, 1));
 		}
 		if (*s1 == '\0')
-		{
 			return (0);
-		}
 		return (wildcmp_helper((s1 + 1), s2, 0));
 	}
 	else
@@ -34,26 +27,18 @@ int wildcmp_helper(char *s1, char *s2, int wildcard)
 		if (*s2 == '*')
 		{
 			if (s2[1] == '*')
-			{
 				return (wildcmp_helper(s1, (s2 + 1), 0));
-			}
 			return (wildcmp_helper(s1, (s2 + 1), 1));
 		}
 		else if (*s1 == *s2)
 		{
 			if (*s1 == '\0')
-			{
 				return (1);
-			}
 			else
-			{
 				return (wildcmp_helper((s1 + 1), (s2 + 1), 0));
-			}
 		}
 		else /* s1 != s2 */
-		{
 			return (0);
-		}
 	}
 }
 
