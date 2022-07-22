4d45d90ba526a64d0dca75f23d4ba64ccacac703
Joseph Alikah <alikahjoseph@gmail.com>
2022-07-22 12:48:20 +0100

    Add task 0

---
+++ 0x0A-argc_argv/0-whatismyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc - Argument count
+ * @argv - Argument vector
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
