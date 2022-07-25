cce6b786205355eefaa5c7743d79860bef201685
Joseph Alikah <alikahjoseph@gmail.com>
2022-07-25 10:53:54 +0100

    Add task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - Find the number of characters in a null-terminated string
+ * @s: String whose characters to count
+ *
+ * Return: The number of characters in s. 0 if s is NULL
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	if (s == NULL)
+	{
+		return (0);
+	}
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
+
+/**
+ * argstostr - Concatenates the arguments of a program
+ * @ac: Argument count
+ * @av: Argument vector
+ *
+ * Return: Pointer to concatenated string
+ */
+char *argstostr(int ac, char **av)
+{
+	unsigned int i, length, count;
+	char *result;
+
+	if (ac < 1 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	length = 0;
+	for (i = 0; i < ac; i++)
+	{
+		length += _strlen(av[i]) + 1;
+	}
+	result = malloc(length);
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	count = 0;
+	for (i = 0; i < ac; i++)
+	{
+		int j, arglen = _strlen(av[i]);
+
+		for (j = 0; j < arglen; j++)
+		{
+			result[count++] = av[i][j];
+		}
+		if (count == length - 1)
+		{
+			result[count++] = '\0';
+		}
+		else
+		{
+			result[count++] = '\n';
+		}
+	}
+
+	return (result);
+}
