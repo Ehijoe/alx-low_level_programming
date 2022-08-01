c1538f59fb9cbcb2619b88e499fa9c49980d5870
Joseph Alikah <alikahjoseph@gmail.com>
2022-08-01 13:49:55 +0100

    Add 0x0E task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A struct for storing info about a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the owner
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif
