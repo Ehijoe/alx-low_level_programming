5cae04e1a90da71a346d21ed0b06fa760442b8b1
Joseph Alikah <alikahjoseph@gmail.com>
2022-08-01 15:45:10 +0100

    Add 0x0# task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Free the memory occupied by a dog
+ * @d: A pointer to the dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d->name != NULL)
+	{
+		free(d->name);
+	}
+	if (d->owner != NULL)
+	{
+		free(d->owner);
+	}
+	free(d);
+}
