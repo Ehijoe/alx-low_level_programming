049b15bab8af754ada9b0e8f1d4a411c451d0d73
Joseph Alikah <alikahjoseph@gmail.com>
2022-08-01 15:40:39 +0100

    Add 0x0# task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,5 +21,8 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+unsigned int _strlen(char *s);
+void _strcpy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
