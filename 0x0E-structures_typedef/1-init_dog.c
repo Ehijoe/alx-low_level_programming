478bd2c70cc1eacda0902931001d6f33079d1bac
Joseph Alikah <alikahjoseph@gmail.com>
2022-08-01 14:53:27 +0100

    Add 0x0E task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initialize a dog struct
+ * @d: A pointer to the dog struct
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+}
