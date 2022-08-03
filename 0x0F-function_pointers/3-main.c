9898eefcb7e37b54e68e73f075a3ed6b95e38b34
Joseph Alikah <alikahjoseph@gmail.com>
2022-08-03 22:35:36 +0100

    Fix 0x0F task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,12 +16,7 @@ int main(int argc, char **argv)
 	if (argc != 4)
 	{
 		puts("Error");
-		return (98);
-	}
-	if (get_op_func(argv[2]) == NULL)
-	{
-		puts("Error");
-		return (98);
+		exit(98);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
