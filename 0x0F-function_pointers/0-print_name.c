a472d875cecaf50c9b431165affd32379f930c7c
Joseph Alikah <alikahjoseph@gmail.com>
2022-08-03 17:28:50 +0100

    Add task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name with the appropriate function
+ * @name: The name to print
+ * @f: The function to use to print name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
