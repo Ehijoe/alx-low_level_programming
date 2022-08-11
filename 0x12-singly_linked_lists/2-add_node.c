31ad672415e33553e28d0fce5df6b4be59931769
Joseph Alikah <alikahjoseph@gmail.com>
2022-08-11 12:47:01 +0100

    0x12 - task 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,6 +2,25 @@
 #include <string.h>
 
 /**
+ * _strlen - Gets the length of a string
+ * @s: The string whose length to find
+ *
+ * Return: The length of s
+ */
+int _strlen(const char *s)
+{
+	if (s == NULL)
+	{
+		return (0);
+	}
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen(s + 1));
+}
+
+/**
  * add_node - Adds a node to the start of the list
  * @head: A pointer to the list(The list is a pointer to the first node)
  * @str: The string to store in the node
@@ -21,7 +40,15 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
-	new->str = strdup(str);
+	if (str == NULL)
+	{
+		new->str = NULL;
+	}
+	else
+	{
+		new->str = strdup(str);
+	}
+	new->len = _strlen(str);
 	new->next = *head;
 	*head = new;
 	return (new);
