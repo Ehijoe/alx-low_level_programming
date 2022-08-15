810a41574e263b122349a342275c446a45a14e71
Joseph Alikah <alikahjoseph@gmail.com>
2022-08-15 13:21:46 +0100

    0x13 - Add task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees the memory occupied by a list
+ * @head: A pointer to the first node in the list
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+	free_listint(head->next);
+	free(head);
+}
+
+/**
+ * free_listint2 - Frees a list and sets the head to NULL
+ * @head: A pointer to the head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	free_listint(*head);
+	*head = NULL;
+}
