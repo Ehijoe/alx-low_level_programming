cdf426ac79ded44fa04ebac8e747be4a8a9c73a7
Joseph Alikah <alikahjoseph@gmail.com>
2022-08-18 11:29:49 +0100

    0x14 - Add task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_binary - Print an unsigned int in binary form
+ * @n: The unsigned int to print
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar('0' + (n & 1));
+}
