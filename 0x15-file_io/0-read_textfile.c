8d2ac963c303855934794b8d00232b60dbc81321
Joseph Alikah <alikahjoseph@gmail.com>
2022-08-22 15:15:43 +0100

    0x15 - Add Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - Writes the content of a file to stdout
+ * @filename: The name of the file to open
+ * @letters: Number of letters to read from the file
+ *
+ * Return: The number of letters read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t len;
+	ssize_t written;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+
+	len = read(fd, buffer, letters);
+	if (len == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	written = write(STDIN_FILENO, buffer, len);
+	if (written != len)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	return (written);
+}
