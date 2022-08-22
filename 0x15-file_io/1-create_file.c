e5ef53f724dedcfd20e06db167d64940569ff173
Joseph Alikah <alikahjoseph@gmail.com>
2022-08-22 16:17:24 +0100

    0x15 - Add Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * create_file - Creates a file and writes to it
+ * @filename: Name of the file to create
+ * @text_content: The text to write to the file
+ *
+ * Return: 1 on success. -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t i;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[i] != '\0')
+	{
+		if (write(fd, (text_content + i), 1) != 1)
+		{
+			close(fd);
+			return (-1);
+		}
+		i++;
+	}
+
+	close(fd);
+	return (1);
+}
