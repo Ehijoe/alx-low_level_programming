fe52e24e712912e936d357b2b6b8e333b86c305b
Joseph Alikah <alikahjoseph@gmail.com>
2022-09-29 13:01:31 +0100

    0x17 - Format task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,5 +14,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (NULL);
 	if (index == 0)
 		return (head);
-	return get_dnodeint_at_index(head->next, index - 1);
+	return (get_dnodeint_at_index(head->next, index - 1));
 }
